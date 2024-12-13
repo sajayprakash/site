2062a1ba164d956384664c3fef04546533e34929
Sajay Prakash <sajayprakashk@gmail.com>
2024-12-13 08:37:12 +0000

    overhaul

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_URL = "https://sajay.dev";
+export const SITE_TITLE = "Sajay Prakash";
+export const SITE_DESCRIPTION =
+  "Full stack developer, digital minimalist, computer science student.";
+
+export const EMAIL = "sajayprakashk@gmail.com";
